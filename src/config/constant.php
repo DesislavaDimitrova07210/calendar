14a9c190832bbbfa57fb30b9a4fb760f3203eaa9
desi dell <desislava07210@gmail.com>
2025-09-21 21:27:16 +0300

    Revert "migration tbl"
    This reverts commit e507605feb8828b3a2abf24e6da62b83163829d3.

--- src/config/constant.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'notification_methods' => [
-        'SMS' => 'SMS',
-        'Email' => 'Email',
-        // 'Push' => 'Push', // we can add here in future
-    ],
-];
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,18 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Web\BookingController;
-
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/list', [BookingController::class, 'index'])->name('bookings.list');
-Route::get('/show/{id}', [BookingController::class, 'show'])->name('bookings.show');
-Route::get('/create', [BookingController::class, 'create'])->name('bookings.create');
-Route::post('/store', [BookingController::class, 'store'])->name('bookings.store');
-Route::get('/edit/{id}', [BookingController::class, 'edit'])->name('bookings.edit');
-Route::put('/update/{id}', [BookingController::class, 'update'])->name('bookings.update');
-Route::delete('/delete/{id}', [BookingController::class, 'destroy'])->name('bookings.destroy');
-
